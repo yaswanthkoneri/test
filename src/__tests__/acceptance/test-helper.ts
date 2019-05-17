3d0af4c9024f6d834f732d1a92daf1b3a1283e96
yaswanth.koneri <yaswanth.koneri@omniwyse.com>
2019-05-17 19:33:18 +0530

    final changes

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {TestAppApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new TestAppApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: TestAppApplication;
+  client: Client;
+}
